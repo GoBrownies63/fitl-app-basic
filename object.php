3f95b051a90be977d0d50a3b00814757ae131ce7
Jake Schultz <jschultz3@gmail.com>
2015-10-09 15:30:12 -0400

    created a database powered dynamic object page

--- object.php
+++ object.php
@@ -2,52 +2,65 @@
 $id = $_REQUEST['id'];
 
 $object = array(
-	'title' => '',
-	'ingredient' => '',
+	'id' => '',
+	'food_type' => '',
+	'brand' => '',
+	'quantity' => '',
 	'description' => '',
-	'code' => '',
-	'date' => '',
 	);
 
-// set the object variables
-// based on the id value from the URL
-if ($id == 1){ 
-	$object = array(
-		'title' => 'A Food Type',
-		'ingredient' => 'Chicken Breast',
-		'description' => 'I need a recipe I can make using boneless
-		chicken breasts.',
-		'code' => 'alert(this is a message',
-		'date' => 'January 11, 2015',
-		);
 
+// Database connection credentials
+$servername = 'localhost';
+$username = 'homestead';
+$password = 'secret';
+
+// Create connection
+$connection = new mysqli($servername, $username, $password);
+
+// Check for an error
+if ($connection->connect_error) {
+	echo 'Connection failed: ' , $connection->connect_error;
+	exit;
 }
 
-elseif($id == 2){
-	$object = array(
-	'title' => 'A Food Type',
-	'ingredient' => 'Hamburger Meat',
-	'description' => 'I need a recipe I can make using
-					ground hamburger meat.',
-	'code' => 'alert(This is a message)',
-	'date' => 'June 1, 2015',
-	);
+// Otherwise, connected successfully!
+// echo 'Connected successfully';
+
+// Connect to the fitl database
+$connection->select_db('fitl');
+
+// Query to select the object
+// SELECT * FROM groceries WHERE id = 1
+$sql = 'SELECT * FROM groceries WHERE id = '. $id;
+
+$result = $connection->query($sql);
+
+// Check for an retrieve the object
+if ($result->num_rows > 0){
+	$object = $result->fetch_assoc();
+	//echo '<pre>';
+	//print_r($object);
+	//echo '</pre>';
 }
+
+
 
 ?>
 <!DOCTYPE html>
 <html>
 	<head>
-		<title><?php echo $object['title']; ?></title>
+		<title><?php echo $object['food_type']; ?></title>
 	</head>
 
 	<body>
 		
-		<h1><?php echo $object['ingredient']; ?></h1>
-			<p><?php echo $object['description']; ?></p>
+		
+			<p><?php echo $object['food_type']; ?></p>
 				<pre>
-					<?php echo $object['code']; ?>
+					<?php echo $object['brand']; ?>
 				</pre>
-				<p>Question Date: <?php echo $object['date']; ?></p>
+				<p><?php echo $object['quantity']; ?></p>
+				<?php echo $object['description']; ?>
 	</body>
 </html>
